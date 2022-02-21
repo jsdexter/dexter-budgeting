c506cccaf352393b40976da052796b1edc76b350
Jason Dexter <45626133+jsdexter@users.noreply.github.com>
2022-02-21 16:48:53 -0500

    updated deploy_stage deploy path

--- deploy/budgeting-webhooks/shell.js
+++ deploy/budgeting-webhooks/shell.js
@@ -32,8 +32,7 @@ handler.on('workflow_run', function (event) {
   if (repository === REPO_NAME && action === 'completed') {
     // we should deploy now
     shell.cd('..');
-    console.log('It make it this far');
-    shell.exec('~/scripts/deploy_stage');
+    shell.exec('~/dexter-budgeting/deploy/scripts/deploy_stage');
     console.log('This is most likely not the finale part 6!!!')
   }
 });
